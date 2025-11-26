1ed7630def2aed3d0cd91f48e4f86083777e4c7d
Shriyansh Chandra <chandrashriyansh@gmail.com>
2025-11-26 15:23:45 +0530

    added phone optimization

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -3,14 +3,20 @@ import { Link, useNavigate } from "react-router-dom";
 import "./Navbar.css";
 
 const Navbar = ({ setSearchQuery }) => {
+  const [isOpen, setIsOpen] = useState(false);
   const [localSearch, setLocalSearch] = useState("");
   const navigate = useNavigate();
+
+  const toggleMenu = () => {
+    setIsOpen(!isOpen);
+  };
 
   const handleSearch = () => {
     if (setSearchQuery) {
       setSearchQuery(localSearch);
     }
     navigate("/books");
+    setIsOpen(false); // Close menu on search
   };
 
   const handleKeyDown = (e) => {
@@ -23,21 +29,49 @@ const Navbar = ({ setSearchQuery }) => {
     <nav className="navbar">
       <div className="navbar-container">
         {/* Logo */}
-        <Link to="/" className="logo-link">
+        <Link to="/" className="logo-link" onClick={() => setIsOpen(false)}>
           <span className="brand-name">ExamFobia</span>
         </Link>
 
+        {/* Hamburger Icon */}
+        <div className="menu-icon" onClick={toggleMenu}>
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            width="30"
+            height="30"
+            viewBox="0 0 24 24"
+            fill="none"
+            stroke="white"
+            strokeWidth="2"
+            strokeLinecap="round"
+            strokeLinejoin="round"
+          >
+            {isOpen ? (
+              <>
+                <line x1="18" y1="6" x2="6" y2="18"></line>
+                <line x1="6" y1="6" x2="18" y2="18"></line>
+              </>
+            ) : (
+              <>
+                <line x1="3" y1="12" x2="21" y2="12"></line>
+                <line x1="3" y1="6" x2="21" y2="6"></line>
+                <line x1="3" y1="18" x2="21" y2="18"></line>
+              </>
+            )}
+          </svg>
+        </div>
+
         {/* Navigation Links - Centered */}
-        <ul className="nav-menu">
-          <li><Link to="/">Home</Link></li>
-          <li><Link to="/books">Books</Link></li>
+        <ul className={isOpen ? "nav-menu active" : "nav-menu"}>
+          <li><Link to="/" onClick={() => setIsOpen(false)}>Home</Link></li>
+          <li><Link to="/books" onClick={() => setIsOpen(false)}>Books</Link></li>
           <li className="dropdown">
             <span>Downloads</span>
             <ul className="dropdown-menu">
-              <li><Link to="/papers">Previous Year Papers</Link></li>
+              <li><Link to="/papers" onClick={() => setIsOpen(false)}>Previous Year Papers</Link></li>
             </ul>
           </li>
-          <li><Link to="/about">About Us</Link></li>
+          <li><Link to="/about" onClick={() => setIsOpen(false)}>About Us</Link></li>
         </ul>
 
         {/* Search Bar - Right Aligned */}
